4ce75bd5f52971b1c7931db220701fe6e5e92c3f
shoiebjaveed <45892207+shoiebjaveed@users.noreply.github.com>
2022-04-11 06:55:52 +0530

    Completed task 4

---
+++ callback.js
@@ -0,0 +1,36 @@
+const posts = [
+        {title:'Post One', body:'This is post one', createdAt: new Date().getTime()},
+        {title: 'Post Two', body:'This is post two', createdAt: new Date().getTime()}
+];
+
+let intervalID = 0;
+function getPosts() {
+        clearInterval(intervalID)
+        intervalID = setInterval(()=>{
+                let output = "";
+                posts.forEach((post, index) =>{
+                        output +=`<li>${post.title} updated ${(new Date().getTime() - post.createdAt)/1000} seconds ago</li>`;
+                });
+                document.body.innerHTML = output;
+        }, 1000)
+        
+}
+
+function createPost(post, callback) {
+        setTimeout(()=>{
+                posts.push({...post, createdAt: new Date().getTime()});
+                callback();
+        }, 2000);
+}
+
+function create4thPost(post, callback){
+        setTimeout(() =>{
+                posts.push({...post, createdAt: new Date().getTime()})
+                callback();
+        }, 3000)
+}
+
+
+createPost({ title: 'Post Three', body: 'This is post three'}, getPosts);
+
+create4thPost({ title: 'Post four', body: 'This is post four'}, getPosts);
